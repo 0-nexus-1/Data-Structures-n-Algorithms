da30183253c431b1d9e2511829102c6a328ce4ca
༺༒Nexu$༒༻ <123191758+0-nexus-1@users.noreply.github.com>
2023-01-21 17:04:41 +0500

    Update Print-without-Recurrsion-n-Loops.cpp

--- Print-without-Recurrsion-n-Loops.cpp
+++ Print-without-Recurrsion-n-Loops.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main()
+{
+    cout << "\n\t\tDate: " << __TIME__ << "\t\tTime: " << __DATE__ << endl
+         << endl;
+    cout << "Printing without Recurrsion and Loops. " << endl;
+    int i = 1;
+
+p:
+    if (i <= 10)
+    {
+        cout << i << endl;
+        i++;
+        goto p;
+    }
+    return 0;
+}
