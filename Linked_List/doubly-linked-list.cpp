82f3f3d294255d0c209f93e0f324d0df18a8b15e
༺༒Nexu$༒༻ <123191758+0-nexus-1@users.noreply.github.com>
2023-01-21 13:33:38 +0500

    Update doubly-linked-list.cpp

--- Linked_List/doubly-linked-list.cpp
+++ Linked_List/doubly-linked-list.cpp
@@ -1,1 +1,112 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+struct node
+{
+    int data;
+    node *next;
+    node *prev;
+};
+
+class DLL
+{
+protected:
+private:
+public:
+    void InsertLast(node *&head, int x);
+    void InsertFirst(node *&head, int x);
+    void display(node *head);
+    bool search(node *head, int num);
+};
+
+void DLL::InsertLast(node *&head, int x)
+{
+    node *n = new node();
+    n->data = x;
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+    n->prev = temp;
+}
+
+void DLL::InsertFirst(node *&head, int x)
+{
+    node *n = new node();
+    n->data = x;
+    n->next = head;
+    if (head != NULL)
+        head->prev = n;
+    head = n;
+}
+
+void DLL::display(node *head)
+{
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        cout << temp->data << " <-> ";
+        temp = temp->next;
+    }
+    cout << temp->data << " <-> NULL" << endl;
+
+    if (temp->prev != NULL)
+        temp = temp->prev;
+    while (temp->prev != NULL)
+    {
+        cout << temp->data << " <-> ";
+        temp = temp->prev;
+    }
+    cout << temp->data << " <-> NULL" << endl;
+}
+
+bool DLL::search(node *head, int num)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        if (temp->data == num)
+        {
+            return true;
+        }
+        temp = temp->next;
+    }
+
+    return false;
+}
+
+int main()
+{
+    DLL s;
+    node *head = NULL;
+
+    cout << "\n\t\t" << __TIME__ << "\t\t" << __DATE__ << endl
+         << endl;
+
+    cout << "\4 ";
+    s.InsertLast(head, 31);
+    s.InsertLast(head, 32);
+    s.InsertLast(head, 33);
+
+    cout << "Inserted at Tail____" << endl;
+    s.display(head);
+    cout << endl;
+
+    cout << "\4 ";
+    s.InsertFirst(head, 34);
+    cout << "Inserted at Head____" << endl;
+    s.display(head);
+    
+    cout << endl;
+    cout << "\4 " << s.search(head, 33) << endl;
+    return 0;
+}
